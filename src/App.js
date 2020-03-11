f5dcc33a6f85347520c1eebbccfbea45a901543f
Yaghmurassen Sainson <yaghmurassen.sainson@autovisual.com>
2020-03-11 18:15:48 +0100

    page transitions

--- src/App.js
+++ src/App.js
@@ -1,43 +1,91 @@
 import React, { useRef, useEffect } from "react";
 import "./App.scss";
 import People from "./images/people.webp";
-import { TimelineLite, Power2 } from "gsap";
+import Back from "./images/back.jpg";
+import { TimelineLite, Power2, Power1 } from "gsap";
 import CSSRulePlugin from "gsap/CSSRulePlugin";
 
 const App = () => {
   let image = useRef(null);
   let container = useRef(null);
-  let imageReveal = CSSRulePlugin.getRule(".img-container:after");
+  let title = useRef(null);
+  // let imageRevealOne = CSSRulePlugin.getRule(".img-container:after");
+  // let imageRevealTwo = CSSRulePlugin.getRule(".img-container:before");
+  let mainReveal = CSSRulePlugin.getRule(".main:before");
 
   let tl = new TimelineLite();
 
   useEffect(() => {
     tl.to(container, 0, { css: { visibility: "visible" } });
-    tl.to(imageReveal, 1.4, { width: "0%", ease: Power2.easeInOut });
-    tl.from(image, 1.4, {
-      scale: 1.6,
+    tl.to(mainReveal, 1.5, {
+      width: "100%",
+      ease: Power1.easeInOut,
+      delay: 0.5
+    });
+    tl.to(".title", 0.2, {
+      // display: "block",
+      opacity: 1
+    });
+    tl.from(".title", 1, {
+      // display: "block !important",
+      // delay: 0.8,
+      ease: "power3.out",
+      // y: 64,
+      height: "15px",
+      zIndex: 9999
+    });
+    tl.to(mainReveal, 2, {
+      left: "0px",
+      width: "0%",
       ease: Power2.easeInOut,
-      delay: -1.4
+      delay: 1
+    });
+    tl.to(".title", 1, {
+      opacity: 0,
+      ease: "power3.out",
+      delay: -2
     });
   });
 
   return (
-    <section className="main">
-      <p>GSAP IMAGE REVEAL</p>
-      <div className="container" ref={el => (container = el)}>
-        <>
-          <div className="img-container">
-            <img
-              ref={el => {
-                image = el;
-              }}
-              src={People}
-            />
-          </div>
-        </>
+    <>
+      <section className="main">
+        <p>GSAP IMAGE REVEAL</p>
+        <div className="container" ref={el => (container = el)}>
+          <>
+            <div className="img-container">
+              <img
+                ref={el => {
+                  image = el;
+                }}
+                src={People}
+                alt="People"
+              />
+            </div>
+          </>
+        </div>
+      </section>
+      <div>
+        <h1 className="title" ref={el => (title = el)}>
+          La Jazzinière
+        </h1>
       </div>
-    </section>
+    </>
   );
 };
 
 export default App;
+
+// tl.to(imageRevealOne, 2, { width: "0%", ease: Power2.easeInOut });
+// tl.from(imageRevealTwo, 2, { width: "100%", ease: Power2.easeInOut });
+// tl.from(
+//   image,
+//   3.5,
+//   {
+//     scale: 1.6,
+//     ease: Power2.easeInOut
+//     // delay: -1
+//   },
+//   "-=5"
+// );
+// tl.from(imageRevealTwo, 1, { width: "100%", ease: Power2.easeInOut });
