31aa2961461eb7b00b62528614336509a0cab50f
Yag <sainsony@gmail.com>
2020-03-19 18:30:57 +0100

    confinement 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,87 @@
 import React, { useRef, useEffect } from "react";
+import { Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Jazz from "./Components/Jazz";
+import Contact from "./Components/Contact";
 import "./App.scss";
 import People from "./images/people.webp";
-import { TimelineLite, Power2 } from "gsap";
+import { TimelineLite, Power2, gsap } from "gsap";
+import { CSSTransition } from "react-transition-group";
+
 import CSSRulePlugin from "gsap/CSSRulePlugin";
+
+const routes = [
+  { path: "/contact", name: "Contact", Component: Contact },
+  { path: "/jazz", name: "Jazz", Component: Jazz }
+];
+
+const onEnter = node => {
+  let tl = new TimelineLite();
+  console.log("onEnter Element : ", document.body.querySelector(".main"));
+  let main = document.body.querySelector(".main:after");
+  let p = CSSRulePlugin.getRule(".main p");
+
+  tl.to(main, 1, { width: "100%" });
+  tl.fromTo(
+    p,
+    1.5,
+    { transform: "translate(-200px)" },
+    { opacity: 1, x: "500px", transform: "translate(200px)" }
+  );
+  tl.to(p, 1, { opacity: 0, transform: "translate(600px)", delay: 1 });
+  tl.to(main, 0, { transform: "scale(2)" });
+  tl.to(
+    main,
+    1.5,
+    {
+      right: "-500px",
+      width: "0%",
+      transform: "skewX(2deg)",
+      height: "200%",
+      ease: Power2.easeOut
+    },
+    "-=1"
+  );
+};
+
+const onExit = node => {
+  console.log("onExit Element : ", node);
+  let tl = new TimelineLite();
+  console.log("ON EXIT", node);
+};
 
 const App = () => {
   let image = useRef(null);
   let container = useRef(null);
   let imageReveal = CSSRulePlugin.getRule(".img-container:after");
+  let mainReveal = CSSRulePlugin.getRule(".main:after");
+  let pReveal = CSSRulePlugin.getRule(".main p");
 
   let tl = new TimelineLite();
 
   useEffect(() => {
+    tl.to(mainReveal, 1, { width: "100%" });
+    tl.fromTo(
+      pReveal,
+      1.5,
+      { transform: "translate(-200px)" },
+      { opacity: 1, x: "500px", transform: "translate(200px)" }
+    );
+    tl.to(pReveal, 1, { opacity: 0, transform: "translate(600px)", delay: 1 });
+    tl.to(mainReveal, 0, { transform: "scale(2)" });
+    tl.to(
+      mainReveal,
+      1.5,
+      {
+        right: "-500px",
+        width: "0%",
+        transform: "skewX(2deg)",
+        height: "200%",
+        ease: Power2.easeOut
+      },
+      "-=1"
+    );
+
     tl.to(container, 0, { css: { visibility: "visible" } });
     tl.to(imageReveal, 1.4, { width: "0%", ease: Power2.easeInOut });
     tl.from(image, 1.4, {
@@ -22,21 +92,42 @@ const App = () => {
   });
 
   return (
-    <section className="main">
-      <p>GSAP IMAGE REVEAL</p>
-      <div className="container" ref={el => (container = el)}>
-        <>
-          <div className="img-container">
-            <img
-              ref={el => {
-                image = el;
-              }}
-              src={People}
-            />
+    <Router>
+      <CSSTransition
+        in={true}
+        timeout={1200}
+        classNames="page"
+        onExit={onExit}
+        onEntering={onEnter}
+        unmountOnExit
+      >
+        <section className="main">
+          <p>GSAP IMAGE REVEAL</p>
+          <div className="container" ref={el => (container = el)}>
+            <>
+              <div className="img-container">
+                <img
+                  ref={el => {
+                    image = el;
+                  }}
+                  src={People}
+                  alt="alt"
+                />
+              </div>
+              <button
+                className="btn btn-jazz"
+                onClick={onEnter}
+                onMouseLeave={onExit}
+              >
+                <Route path="/jazz" component={Jazz} className="jazz">
+                  Voir la culture
+                </Route>
+              </button>
+            </>
           </div>
-        </>
-      </div>
-    </section>
+        </section>
+      </CSSTransition>
+    </Router>
   );
 };
 
---
+++ src/Components/Contact.js
@@ -0,0 +1,19 @@
+import React, { useRef, useEffect } from "react";
+import "../App.scss";
+import { TimelineLite, Power2 } from "gsap";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+
+const Contact = () => {
+  return (
+    <div className="inner">
+      {/* <Title lineContent="How we get" lineContent2="things done" /> */}
+      <p className="other">
+        A character his cache I succeed employed entire been it find the more
+        and may the to his their five and towards in lay rippedup, what and so
+        endure before for her been decades the few to than would was concept.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
